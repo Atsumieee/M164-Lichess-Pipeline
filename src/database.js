23f6a28a1cfb79e53a7b521a4a8542a71abdc0ca
Atsumieee <228591627+Atsumieee@users.noreply.github.com>
2026-09-08 13:24:56 +0200

    Reapply "Chore: Test Commit"
    This reverts commit 929f1abf60bb605810fecc9c570d51c58e8c857f.

--- src/database.js
+++ src/database.js
@@ -281,6 +281,7 @@ async function mergeImportData(tournaments, players, games, standings) {
     insertPlayers(players);
     insertGames(games);
     insertStandings(standings);
+    // test
   });  
 
   ExecImport(tournaments, players, games, standings);
